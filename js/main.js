08b37d4d0c018d2de860360e6c444f90965ca32d
broboredo <roboredo.bruno@gmail.com>
2015-04-07 03:32:20 -0300

    Alteração e Criação
    Adicionando a class inside para a função onScroll (qualquer link do site
    que tiver a class inside funcionará como onscroll)
    Melhorado função criaSpan
    Criando função ajax para os formulários restantes

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ $(document).ready(function($) {
 	
 	//SCROLL
 	$(document).on("scroll", onScroll);
-
-	$('#main_menu2 ul li a[href^="#"]').on('click', function (e) {
+    
+	$('body a[href^="#"].inside').on('click', function (e) {
+        if(!e)
+            e = window.event;
 		e.preventDefault();
 		$(document).off("scroll");
 
@@ -47,24 +49,33 @@ $(document).ready(function($) {
         return regex.test(email);
     }
     
-    function criaSpan(msg){
-        var form = $("form").parents();
-        form.prepend("<span class='formError'></span>");
-        $("span.formError").html(msg);
-        $("span.formError").remove().delay(10000);
+    function criaSpan(msg, este){
+        var form = este.parents("form");
+        if(form.find("span.formError").length <= 0)
+        {
+            form.append("<span class='formError'></span>");
+        }
+        $("span.formError").html(msg).addClass('wow fadeInLeft animated');
+        setTimeout(function(){
+            $("span.formError").removeAttr("style").addClass('wow fadeOutLeft animated');            
+            setTimeout(function(){
+                $("span.formError").remove();
+            }, 1000);
+        }, 3000);
     }
     
+    //***FORM DO E-MAIL****//
     $("#form-email button[type='submit']").on("click", function(){
         var button = $(this);
         var email = $('#form-email input[type="email"]').val();
-        var data = {
+        var dados = {
             form: 'form-email',
             email: email
-        };        
-        data = $(this).serialize() + "&" + $.param(data);
+        };
+        dados = $(this).serialize() + "&" + $.param(dados);
         if(!validEmail(email) || email == ''){
-            criaSpan("E-mail inválido.");
-            return false;       
+            criaSpan("E-mail inválido.", button);
+            return false;
         }
          $.ajax({
              beforeSend: function() {
@@ -74,53 +85,54 @@ $(document).ready(function($) {
             },
             url : "ajax/ajax.php",
             type: 'post',
-            data: data,            
+            data: dados,
+            dataType: 'json',
             success : function(data){
                 if(data['return'] === true)
                 {
-                    $('#form-more input#email').val(data['email']);
-                    setTimeout(
-                        function(){
-                            button.children('img').addClass('hidden');
-                            button.children('span').html("Ok!").delay(5000).removeClass('hidden');
-                            $('#form-email').removeAttr('style');
-                        }, 2000);
-                    setTimeout(
-                        function(){
-                            $('#form-email').addClass('wow animated flipOutX');
-                        }, 2000);
-                    setInterval(function(){
+                    $('#form-more input#id_email').val(data['id']);
+                    $('#form-friend input#id_email').val(data['id']);
+                    button.children('img').addClass('hidden');
+                    button.children('span').html("Ok!").removeClass('hidden');
+                    $('#form-email').removeAttr('style');
+                    setTimeout(function(){
+                        $('#form-email').addClass('wow animated flipOutX');
                         $('#form-email').addClass('hidden');
                         $('#form-more').removeClass('hidden');
-                        $('#form-more').addClass('wow flipInX animated');                
-                    }, 2300);
+                        $('#form-more').addClass('wow flipInX animated');
+                    }, 1300); 
                 } else {
                     button.removeAttr('disabled');
                     button.children('span').removeClass('hidden');
                     button.children('img').addClass('hidden');
+                    criaSpan("Ops, ocorreu algum erro. Tente novamente!", button)
                     return false;
                 }
             },             
-            error: function(error) { 
-                alert(error);
+            error: function(error) {
+                alert('fodase ' + $(this));
+                criaSpan("Ocorreu algum erro.", button);
+                return false;
             },
          });
         return false;
     });
     
+    //***FORM DO MORE****//
     $("#form-more button[type='submit']").on("click", function(){
         var button = $(this);
-        var nome = $('#form-more #nome').val();
-        var telefone = $('#form-more #telefone').val();
-        var mensagem = $('#form-more #mensagem').val();
-        var data = {
+        var id_email = $('#form-more input#id_email').val();
+        var nome = $('#form-more input#nome').val();
+        var telefone = $('#form-more input#telefone').val();
+        var mensagem = $('#form-more textarea#mensagem').val();
+        var dados = {
             form: 'form-more',
+            id_email: id_email,
             nome: nome,
             telefone: telefone,
             mensagem: mensagem
         };
-        data = $(this).serialize() + "&" + $.param(data);     
-        
+        dados = $(this).serialize() + "&" + $.param(dados);
          $.ajax({
              beforeSend: function() {
                 button.attr('disabled','disabled');
@@ -129,42 +141,93 @@ $(document).ready(function($) {
             },
             url : "ajax/ajax.php",
             type: 'post',
-            data: data,            
+            data: dados,
+            dataType: 'json',
             success : function(data){
                 if(data['return'] === true)
                 {
-                    $('#form-more input#email').val(data['email']);
-                    setTimeout(
-                        function(){
-                            button.children('img').addClass('hidden');
-                            button.children('span').html("Ok!").delay(5000).removeClass('hidden');
-                            $('#form-email').removeAttr('style');
-                        }, 2000);
-                    setTimeout(
-                        function(){
-                            $('#form-email').addClass('wow animated flipOutX');
-                        }, 2000);
-                    setInterval(function(){
-                        $('#form-email').addClass('hidden');
-                        $('#form-more').removeClass('hidden');
-                        $('#form-more').addClass('wow flipInX animated');                
-                    }, 2300);
+                    button.children('img').addClass('hidden');
+                    button.children('span').html("Ok!").delay(5000).removeClass('hidden');
+                    $('#form-more').removeAttr('style');
+                    setTimeout(function(){
+                        $('#form-more').addClass('wow animated flipOutX');
+                        $('#form-more').addClass('hidden');
+                        $('div.sucesso').removeClass('hidden');
+                        $('div.sucesso h2').addClass('wow fadeInDown animated');
+                        $('div.sucesso h3').addClass('wow fadeInDown animated');
+                    }, 1300);                    
+                    setTimeout(function(){
+                        $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+                    }, 2000);
                 } else {
                     button.removeAttr('disabled');
                     button.children('span').removeClass('hidden');
                     button.children('img').addClass('hidden');
-                    criaSpan("Ocorreu algum erro. Tente novamente.");
+                    criaSpan("Ops, ocorreu algum erro. Tente novamente!", button);
                     return false;
                 }
             },             
             error: function(error) { 
-                alert(error);
+                criaSpan("Ocorreu algum erro.", button);
+                return false;
             },
          });
         return false;
     });
     
-    
+    //***FORM DO AMIGO****//
+    $("#form-friend button[type='submit']").on("click", function(){
+        var button = $(this);
+        var id_email = $('#form-friend input#id_email').val();
+        var email = $('#form-friend input#email').val();
+        var dados = {
+            form: 'form-friend',
+            id_email: id_email,
+            email: email
+        };
+        dados = $(this).serialize() + "&" + $.param(dados);
+        if(!validEmail(email) || email == ''){
+            criaSpan("E-mail inválido.", button);
+            return false;
+        }
+         $.ajax({
+             beforeSend: function() {
+                button.attr('disabled','disabled');
+                button.children('span').addClass('hidden');
+                button.children('img').removeClass('hidden');
+            },
+            url : "ajax/ajax.php",
+            type: 'post',
+            data: dados,
+            dataType: 'json',
+            success : function(data){
+                if(data['return'] === true)
+                {
+                    button.children('img').addClass('hidden');
+                    button.children('span').html("Ok!").delay(5000).removeClass('hidden');
+                    $('#form-friend').removeAttr('style');
+                    setTimeout(function(){
+                        $('#form-friend').addClass('wow animated flipOutX');
+                        $('#form-friend').addClass('hidden');
+                        $('div.sucesso-amigo').removeClass('hidden');
+                        $('div.sucesso-amigo h2').addClass('wow fadeIn animated');
+                        $('div.sucesso-amigo h3').addClass('wow fadeIn animated');
+                    }, 1300);
+                } else {
+                    button.removeAttr('disabled');
+                    button.children('span').removeClass('hidden');
+                    button.children('img').addClass('hidden');
+                    criaSpan("Ops, ocorreu algum erro. Tente novamente!", button);
+                    return false;
+                }
+            },             
+            error: function(error) { 
+                criaSpan("Ocorreu algum erro.", button);
+                return false;
+            },
+         });
+        return false;
+    });
     
     
 	$(".back_top").click(function(event) {
