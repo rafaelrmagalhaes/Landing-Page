8df189c56794c59b8553b5e8099a59d8070aa6e8
broboredo <roboredo.bruno@gmail.com>
2015-04-09 10:50:11 -0300

    Criação
    Criando função para mandar e-mail para o ZeEncontra e para quem se
    registra quando o form der sucesso

--- ajax/ajax.php
+++ ajax/ajax.php
@@ -5,8 +5,10 @@ $return['return'] = false;
 $request = $_REQUEST;
 
 function enviaEmail($msg, $return, $emails){
+    $assunto = "ZeEncontra.com"
     if(!emails)
         $emails = "roboredo.bruno@gmail.com;rafaremagalhaes@gmail.com;contato@zeencontra.com";
+        $assunto = "ERROR";
     if(!$return)
         $msg. = "\r\n";
         $msg. = "Return: \r\n";
@@ -19,7 +21,7 @@ function enviaEmail($msg, $return, $emails){
     $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
     $headers .= "From: no-reply@zeencontra.com\r\n";
     $headers .= "Return-Path: no-reply@zeencontra.com\r\n";
-    mail($emails, "ERROR", $msg, $headers);
+    mail($emails, $assunto, $msg, $headers);
 }
 
 if($request['form'] == 'form-email'){
@@ -29,7 +31,7 @@ if($request['form'] == 'form-email'){
 } else if($request['form'] == 'form-friend'){
     formFriend($request, $return);
 } else {
-    function enviaEmail("O site está fazendo alguma requisição de um form que não existe usando o arquivo ajax.", $return);
+    enviaEmail("O site está fazendo alguma requisição de um form que não existe usando o arquivo ajax.", $return);
     echo json_encode($return);
     return json_encode($return);
 }
@@ -41,7 +43,7 @@ function formEmail($request, $return)
         $return['return'] = true; $return['email'] = $email;
         $result = mysql_query("SELECT email FROM emails WHERE email='".$email."'");
         if(!$result)
-            function enviaEmail("Erro na query que verifica se o email do primeiro form já está salvo no banco.", $return);
+            enviaEmail("Erro na query que verifica se o email do primeiro form já está salvo no banco.", $return);
         $num = mysql_num_rows($result);	
         if($num<=0)
         {
@@ -49,17 +51,19 @@ function formEmail($request, $return)
             $time = date("Y-m-d H:i:s");
             $sql = mysql_query("INSERT INTO emails (id, email, createdAt) VALUES ('".$id."', '".$email."', '".$time."')") or die(mysql_error());
             if(!sql)
-                function enviaEmail("Erro na inserção de dados no primeiro formulário (#form-email).", $return);
+                enviaEmail("Erro na inserção de dados no primeiro formulário (#form-email).", $return);
             $id = mysql_query("SELECT id FROM emails WHERE email='".$email."'");
             $id = mysql_fetch_object($id);
             $id = $id->id;
             $return['id'] = $id;
+            enviaEmail("<center><h3>Valeu por se cadastrar!</h3><p>Aguarde por novidades ;)</p></center>", $return, $email);
+            enviaEmail("Mais um cadastro efetuado no ZeEncontra.com. E-mail: ".$email, $return);
             echo json_encode($return);
             return json_encode($return);
         } else {
             $id = mysql_query("SELECT id FROM emails WHERE email='".$email."'");
             if(!$id)
-                function enviaEmail("Erro ao tentar pegar id da tabela emails na function formEmail (primeira função do ajax).", $return);
+                enviaEmail("Erro ao tentar pegar id da tabela emails na function formEmail (primeira função do ajax).", $return);
             $id = mysql_fetch_object($id);
             $id = $id->id;
             $return['id'] = $id;
@@ -67,7 +71,7 @@ function formEmail($request, $return)
             return json_encode($return);
         }
     }else{
-        function enviaEmail("Primeiro formulário (#form-email) aceitou passar campo email vazio.", $return);
+        enviaEmail("Primeiro formulário (#form-email) aceitou passar campo email vazio.", $return);
         echo json_encode($return);
         return json_encode($return);
     }
@@ -90,11 +94,11 @@ function formMore($request, $return)
             ('".$id."', '".$id_email."', '".utf8_encode($nome)."', '".$telefone."', '".utf8_encode($mensagem)."', '".$time."')")
             or die(mysql_error());
         if(!sql)
-            function enviaEmail("Erro na inserção de dados no segundo formulário (#form-more).", $return);
+            enviaEmail("Erro na inserção de dados no segundo formulário (#form-more).", $return);
         echo json_encode($return);
         return json_encode($return);
     }else{
-        function enviaEmail("Segundo formulário (#form-more) aceitou passar campo telefone vazio OU não conseguiu pegar o id do primeiro form (#form-email)", $return);
+        enviaEmail("Segundo formulário (#form-more) aceitou passar campo telefone vazio OU não conseguiu pegar o id do primeiro form (#form-email)", $return);
         echo json_encode($return);
         return json_encode($return);
     }
@@ -110,11 +114,13 @@ function formFriend($request, $return)
         $time = date("Y-m-d H:i:s");
         $sql = mysql_query("INSERT INTO friend (id, id_email, email_amigo, createdAt) VALUES ('".$id."', '".$id_email."', '".$email."', '".$time."')") or die(mysql_error());
         if(!sql)
-            function enviaEmail("Erro na inserção de dados no formulário indique (#form-friend).", $return);
+            enviaEmail("Erro na inserção de dados no formulário indique (#form-friend).", $return);
+        enviaEmail("<center><h3>Olá!</h3><p>Você foi indicado para conhecer o ZéEncontra.com!</p><p>Acesse <a href='http://www.zeencontra.com' targe='_blank'>CLICANDO AQUI</a> e venda rápido e muito mais!</p></center>", $return, $email);
+        enviaEmail("Mais um amigo foi indicado no ZeEncontra.com. E-mail do amigo: ".$email, $return);
         echo json_encode($return);
         return json_encode($return);
     }else{
-        function enviaEmail("Formulário indique um amigo (#form-friend) aceitou passar campo email vazio OU não conseguiu pegar o id do form (#form-email)", $return);
+        enviaEmail("Formulário indique um amigo (#form-friend) aceitou passar campo email vazio OU não conseguiu pegar o id do form (#form-email)", $return);
         echo json_encode($return);
         return json_encode($return);
     }
